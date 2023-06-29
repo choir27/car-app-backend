5f49df3bb7d1b0b7747ce59fb49784a5389e1a4d
choir24 <richardchoi54@gmail.com>
2023-06-29 14:27:55 -0400

    add delete User route/controller through appwrite

--- controllers/client.js
+++ controllers/client.js
@@ -1,22 +1,32 @@
 const sdk = require('node-appwrite');
 
+ // Initialize SDK
+ const client = new sdk.Client();
+
+ client
+     .setEndpoint(process.env.ENDPOINT) // Your API Endpoint
+     .setProject(process.env.PROJECT) // Your project ID
+     .setKey(process.env.API_KEY) // Your Appwrite API key
+ ;
+
+ const users = new sdk.Users(client);
+
 module.exports = {
     getUsers: async (req, res) => {
         try {
-            // Initialize SDK
-            const client = new sdk.Client();
-
-            client
-                .setEndpoint(process.env.ENDPOINT) // Your API Endpoint
-                .setProject(process.env.PROJECT) // Your project ID
-                .setKey(process.env.API_KEY) // Your Appwrite API key
-            ;
-
-            const users = new sdk.Users(client);
             const response = await users.list();
             res.json(response);
             
         } catch (err) {
+            console.error(err);
+            res.status(500).send(err);
+        }
+    },
+    deleteUser: async (req, res) =>{
+        try{
+            const promise = users.delete(req.params.id);
+            console.log(promise)
+        }catch(err){
             console.error(err);
             res.status(500).send(err);
         }
--- routes/client.js
+++ routes/client.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const clientController = require("../controllers/client");
 
 router.get("/getUsers", clientController.getUsers)
+router.get("/deleteUser/:id", clientController.deleteUser)
 
 module.exports = router;
