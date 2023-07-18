1064d56705725122d68ba5cce8accccb69d1da71
choir24 <richardchoi54@gmail.com>
2023-07-18 17:19:43 -0400

    change from get to delete request for delete user

--- routes/client.js
+++ routes/client.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const clientController = require("../controllers/client");
 
 router.get("/getUsers", clientController.getUsers)
-router.get("/deleteUser/:id", clientController.deleteUser)
+router.delete("/deleteUser/:id", clientController.deleteUser)
 router.get("/setCookie", clientController.setCookie)
 
 module.exports = router;
