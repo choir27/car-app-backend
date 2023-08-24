9ef1aac2fbe36775a9f0a535e672dba779bf9633
choir24 <richardchoi54@gmail.com>
2023-08-24 19:15:24 -0400

    add send email for sending estimate data to client

--- controllers/client.js
+++ controllers/client.js
@@ -70,5 +70,43 @@ module.exports = {
             console.error(err);
             res.status(500).send(err);
         }
+    },
+    sendEstimateEmail: async (req, res) =>{
+        try{
+
+            
+            const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI)
+            oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN});
+
+            const accessToken = await oAuth2Client.getAccessToken();
+
+            const transport = nodemailer.createTransport({
+                service: "gmail",
+                auth:{
+                    type: "OAuth2",
+                    user: "richardchoi54@gmail.com",
+                    clientId: process.env.CLIENT_ID,
+                    clientSecret: process.env.CLIENT_SECRET,
+                    refreshToken: process.env.REFRESH_TOKEN,
+                    accessToken: accessToken
+                }
+            })
+
+            const mailOptions = {
+                from: "Richard Choi <richardchoi54@gmail.com>",
+                to: req.body.email,
+                subject: `Your estimate is ready, ${req.body.firstName} ${req.body.lastName}!`,
+                text:  `Your estimate is ready, ${req.body.firstName} ${req.body.lastName}!`,
+                html: `<h1>Your estimate is ready, ${req.body.firstName} ${req.body.lastName}!</h1><br><p>Completing a ${req.body.service} on your ${req.body.carYear} ${req.body.carMake} ${req.body.carModel} costs a total of ${req.body.price}.  If you have any additional questions, comments or concerns, feel free to contact us at fake-number-9999, or at our email at fake_email_@fakeEmail.com</p><br><br><h3>-AutoAligners</h3>`
+            }
+            
+            const result = await transport.sendMail(mailOptions)
+
+            res.status(500).send("Message sent: " + result);
+
+        }catch(err){
+            console.error(err);
+            res.status(500).send(err);
+        }
     }
 }
