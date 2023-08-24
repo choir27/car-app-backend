779f1600e6c5169626c7d6f695703069d7623768
choir24 <richardchoi54@gmail.com>
2023-08-24 19:27:46 -0400

    add router route for send Estimate Email

--- routes/client.js
+++ routes/client.js
@@ -5,4 +5,6 @@ const clientController = require("../controllers/client");
 router.get("/getUsers", clientController.getUsers)
 router.delete("/deleteUser/:id", clientController.deleteUser)
 router.post("/sendEmail", clientController.sendEmail)
+router.post("/sendEstimateEmail", clientController.sendEstimateEmail)
+
 module.exports = router;
